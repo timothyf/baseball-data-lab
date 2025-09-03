dbc1ece14fa8dd3167b4e4a450ea952aa0263d49
Timothy Fisher <timothyf@gmail.com>
2025-09-03 16:17:36 -0400

    Refactor PlayerInfo to dataclass

--- baseball_data_lab/player/player_info.py
+++ baseball_data_lab/player/player_info.py
@@ -1,56 +1,40 @@
-import json
-from baseball_data_lab.utils import Utils
+"""Player information model."""
+
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
 
 
+@dataclass
 class PlayerInfo:
+    mlbam_id: Optional[int] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    link: Optional[str] = None
+    active: Optional[bool] = None
+    primary_position: Optional[str] = None
+    use_name: Optional[str] = None
 
-    def __init__(self) -> None:
-        self.mlbam_id = None
-        self.first_name = None
-        self.last_name = None
-        self.link = None
-        self.active = None
-        self.primary_position = None
-        self.use_name = None
+    @classmethod
+    def from_mlb_info(cls, mlb_player_info: Dict[str, Any]) -> "PlayerInfo":
+        """Create a ``PlayerInfo`` instance from MLB API data."""
+        return cls(
+            mlbam_id=mlb_player_info.get("id"),
+            first_name=mlb_player_info.get("firstName"),
+            last_name=mlb_player_info.get("lastName"),
+            link=mlb_player_info.get("link"),
+            active=mlb_player_info.get("active"),
+            primary_position=mlb_player_info.get("primaryPosition", {}).get("abbreviation"),
+            use_name=mlb_player_info.get("useName"),
+        )
 
-    def set_from_mlb_info(self, mlb_player_info):
-        self.mlbam_id = mlb_player_info.get('id')
-        self.first_name = mlb_player_info.get('firstName')
-        self.last_name = mlb_player_info.get('lastName')
-        self.link = mlb_player_info.get('link')
-        self.active = mlb_player_info.get('active')
-        self.primary_position = mlb_player_info.get('primaryPosition', {}).get('abbreviation')
-        self.use_name = mlb_player_info.get('useName')
-
-
-    def to_json(self):
-        player_info = {
+    def to_json(self) -> Dict[str, Any]:
+        return {
             "mlbam_id": self.mlbam_id,
             "first_name": self.first_name,
             "last_name": self.last_name,
             "link": self.link,
             "active": self.active,
             "primary_position": self.primary_position,
-            "use_name": self.use_name
+            "use_name": self.use_name,
         }
-        return player_info #json.dumps(player_info, indent=4, cls=Utils.NumpyEncoder)
 
-    # "player_info": {
-    #     "first_name": "Tarik",
-    #     "last_name": "Skubal",
-    #     "link": "/api/v1/people/669373",
-    #     "active": true,
-    #     "primary_position": {
-    #         "code": "1",
-    #         "name": "Pitcher",
-    #         "type": "Pitcher",
-    #         "abbreviation": "P"
-    #     },
-    #     "use_name": "Tarik",
-    #     "use_last_name": "Skubal",
-    #     "middle_name": "Daniel",
-    #     "boxscore_name": "Skubal",
-    #     "strike_zone": {
-    #         "top": 3.49,
-    #         "bottom": 1.601
-    #     }
